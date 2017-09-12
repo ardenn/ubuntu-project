ea43708a1f1be22014145c5d6492b3023e76b8ec
ardenn <mc_alila@outlook.com>
2017-09-12 16:59:50 +0000

    Modify script to use title() and random.sample

--- pitch_generator.py
+++ pitch_generator.py
@@ -9,24 +9,26 @@ import random
 # print details of EIT up for a pitch
 # group the eits into nexus and ubuntu classes
 '''
-klass = input('Enter class name: ')
+klass = input('Enter class name: ').title()
 
-while klass.lower() not in ('ubuntu', 'nexus'):
-	print('Like seriously? "{}" is not a valid class name'.format(klass))
-	klass = input('Enter class name: ')
+while klass not in ('Ubuntu', 'Nexus'):
+    print('Like seriously? "{}" is not a valid class name'.format(klass))
+    klass = input('Enter class name: ').title()
 
 no_of_pitchees = int(input('How many pitchees do you want? '))
 
 with open('eits-2018.csv') as file_handler:
-	reader = csv.DictReader(file_handler)
-	# eits = [eit for eit in reader] # list comprehension
-	eits = []
+    reader = csv.DictReader(file_handler)
+    # eits = [eit for eit in reader] # list comprehension
+    #eits = [eit for eit in reader]
 
-	for eit in reader:
-		if eit['klass'].lower() == klass:
-			eits.append(eit)
+    for eit in reader:
+        if eit['klass'] == klass:
+            eits.append(eit)
 
-	for i in range(no_of_pitchees):
-		pitchee = random.choice(eits)
+    pitchees = random.sample(eits, no_of_pitchees)
 
-		print("\nName: {name}\nClass: {klass}\nCountry: {country}\nGender: {gender}\n".format(**pitchee))
+    for pitchee in pitchees:
+        print(
+            "\nName: {name}\nClass: {klass}\nCountry: {country}\nGender: {gender}\n".format(
+                **pitchee))
